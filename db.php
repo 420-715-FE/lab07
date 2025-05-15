4e4561812d7d52e4085a85b8f3911b2028f2703b
Pier-Luc Brault <pier-luc@brault.me>
2025-05-15 14:44:16 -0400

    Ajouter deuxième setAttribute à db

--- db.php
+++ db.php
@@ -9,5 +9,6 @@ $dbConfig = [
 
 $db = new PDO("mysql:host={$dbConfig['hote']};dbname={$dbConfig['nomBD']};charset=utf8", $dbConfig['nomUtilisateur'], $dbConfig['motDePasse']);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 ?>
